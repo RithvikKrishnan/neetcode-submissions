6f813ae717580a3fc859b54c206387337f03bf1e
RithvikKrishnan <70037200+RithvikKrishnan@users.noreply.github.com>
2026-05-27 23:18:30 -0400

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        counts = defaultdict(int)
+        for num in nums:
+            counts[num] += 1
+        sortedNums = []
+        for i in range(-50000, 50001):
+            if i in counts:
+                for k in range(counts[i]):
+                    sortedNums.append(i)
+        return sortedNums
+
+        
